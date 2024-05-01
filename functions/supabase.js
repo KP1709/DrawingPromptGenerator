e523824db05e20a7d30422b5ebcc595a11af3333
Kareena Patel <kareenapat17@gmail.com>
2024-05-01 17:57:05 +0100

    Place functions folder outside for development

---
+++ functions/supabase.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+dotenv.config()
+const {
+    VITE_APP_SUPABASE_API_KEY
+} = process.env;
+
+// Connect to our database 
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient("https://whaftqpyevfgxqxdfixi.supabase.co", VITE_APP_SUPABASE_API_KEY);
+
+// Our standard serverless handler function
+export default async event => {
+
+  let { data, error } = await supabase
+    .from('DrawingPrompts')
+    .select('*')
+
+    return Response.json({data})
+  
+}
