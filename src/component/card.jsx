e2294a03a4878ff6e7ea7cb3f66d5df05642d22c
Kareena Patel <kareenapat17@gmail.com>
2024-09-28 20:08:39 +0100

    Clean up code

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -3,15 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiceFive } from "@fortawesome/free-solid-svg-icons/faDiceFive";
 
 const Card = (props) => {
-    return(
+    const { id, promptText, photoAlt, photographer, photographerURL, photoURL } = props.selectedPrompt
+    const { getRandomPrompt } = props
+    
+    return (
         <div className="col card">
-                <h3 className="card__promptNumber">Drawing Prompt #{props.selectedPrompt.id}</h3>
-                <h2 className="card__promptWord">{props.selectedPrompt.promptText}</h2>
-                <img src={props.selectedPrompt.photoURL} alt={props.selectedPrompt.photoAlt} />
-                <h2 className="card__photographer">Photo by {props.selectedPrompt.photographer} on Pexels</h2>
-                <a href={props.selectedPrompt.photographerURL}>View {props.selectedPrompt.photographer}'s profile here</a>
-                <button onClick={() => props.getRandomPrompt()} aria-label="Generate new prompt"><FontAwesomeIcon icon={faDiceFive} /></button>
-            </div>
+            <h3 className="card__promptNumber">Drawing Prompt #{id}</h3>
+            <h2 className="card__promptWord">{promptText}</h2>
+            <img src={photoURL} alt={photoAlt} />
+            <h2 className="card__photographer">Photo by {photographer} on Pexels</h2>
+            <a href={photographerURL}>View {photographer}'s profile here</a>
+            <button onClick={() => getRandomPrompt()} aria-label="Generate new prompt"><FontAwesomeIcon icon={faDiceFive} /></button>
+        </div>
     )
 }
 
