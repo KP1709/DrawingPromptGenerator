b57c7126f2d5ee31528b4a23d8ff1bde341d8e00
Kareena Patel <kareenapat17@gmail.com>
2025-10-19 14:52:35 +0100

    Clean up code and add support for local data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react"
 import { createClient } from "@supabase/supabase-js";
 import UnableToConnect from "./reusableFunctions/UnableToConnect";
+import { data as localData } from "./data/localData.js"
 import Card from "./component/card"
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
 export default function App() {
+    const [isLocalData,] = useState(false) // Change here to use local data
     const [promptLoaded, setPromptLoaded] = useState(false)
-    const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * (30 - 1 + 1) + 1))
+    const [numberOfPrompts, setNumberOfPrompts] = useState(0)
+    const [randomNumber, setRandomNumber] = useState(0)
     const [selectedPrompt, setSelectedPrompt] = useState({
         id: randomNumber,
         promptText: "",
@@ -19,6 +21,33 @@ export default function App() {
         photographer: "",
         photographerURL: ""
     })
+
+    useEffect(() => {
+        async function getNumberOfPrompts() {
+            try {
+                const { data } = await supabase
+                    .from('DrawingPrompts')
+                    .select('id');
+
+                setNumberOfPrompts(data.length)
+            }
+            catch (err) {
+                UnableToConnect()
+            }
+        }
+        if (!isLocalData) {
+            getNumberOfPrompts()
+        }
+        else if (isLocalData) {
+            setNumberOfPrompts(localData.length)
+        }
+    }, [isLocalData])
+
+    useEffect(() => {
+        if (numberOfPrompts) {
+            getRandomPrompt()
+        }
+    }, [numberOfPrompts])
 
     useEffect(() => {
         async function getPrompt() {
@@ -45,15 +74,37 @@ export default function App() {
                 UnableToConnect()
             }
         }
-        getPrompt()
+
+        function getPromptLocalData() {
+            setPromptLoaded(false)
+            const promptData = localData.find(prompt => prompt.id === randomNumber)
+            setSelectedPrompt(prevData => ({
+                ...prevData,
+                id: randomNumber,
+                promptText: promptData.promptText,
+                photoURL: promptData.photoURL,
+                photoAlt: promptData.photoAlt,
+                photographer: promptData.photographer,
+                photographerURL: promptData.photographerURL
+            }))
+            setPromptLoaded(true) // Set this to false to simulate retry interface if using local data
+        }
+
+        if (randomNumber && !isLocalData) {
+            getPrompt()
+        }
+        else if (randomNumber && isLocalData) {
+            getPromptLocalData()
+        }
     }, [randomNumber])
 
     function getRandomPrompt() {
-        setRandomNumber(Math.floor(Math.random() * (30 - 1 + 1) + 1))
+        setRandomNumber(Math.floor(Math.random() * (numberOfPrompts - 1 + 1) + 1))
     }
 
-    function MainDisplay() {
+    const MainDisplay = () => {
         const [showRetry, setShowRetry] = useState(false)
+
         if (promptLoaded && !showRetry) {
             return <Card
                 key={selectedPrompt.id}
---
+++ src/data/localData.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        "id": 1,
+        "promptText": "Cooking",
+        "photoURL": "https://images.pexels.com/photos/954677/pexels-photo-954677.jpeg",
+        "photoAlt": "Time Lapse Photography of Four Black Metal Cooking Wares",
+        "photographer": "Kyla Rose Rockola",
+        "photographerURL": "https://www.pexels.com/@kylarose"
+    },
+    {
+        "id": 2,
+        "promptText": " Treehouse",
+        "photoURL": "https://images.pexels.com/photos/4577433/pexels-photo-4577433.jpeg",
+        "photoAlt": "Rocky coast with vegetation and treehouse",
+        "photographer": "Rachel Claire",
+        "photographerURL": "https://www.pexels.com/@rachel-claire"
+    },
+    {
+        "id": 3,
+        "promptText": "Books",
+        "photoURL": "https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg",
+        "photoAlt": "Books in Black Wooden Book Shelf",
+        "photographer": "Pixabay",
+        "photographerURL": "https://www.pexels.com/@pixabay"
+    }
+]
